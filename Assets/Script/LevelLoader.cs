cb7c80d5681189043d89ae32b894644a858abf28
Vũ Thanh Lâm <79557871+FakeMG@users.noreply.github.com>
2023-06-03 00:28:01 +0700

    feat: win, lose UI

--- Assets/Script/LevelLoader.cs
+++ Assets/Script/LevelLoader.cs
@@ -5,4 +5,12 @@ public class LevelLoader : MonoBehaviour {
     public void LoadLevel(string levelName) {
         SceneManager.LoadSceneAsync(levelName);
     }
+    
+    public void LoadNextLevel() {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void ReloadLevel() {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+    }
 }
---
+++ Assets/Script/Score.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Script {
+    public class Score : MonoBehaviour {
+        [SerializeField] private TextMeshProUGUI scoreText;
+        [SerializeField] private UnityEvent onWin;
+        private int _currentScore;
+
+        private void Update() {
+            UpdateScoreText();
+            if (_currentScore == 10)
+                onWin?.Invoke();
+        }
+
+        public void AddScore(int score) {
+            _currentScore += score;
+        }
+
+        private void UpdateScoreText() {
+            scoreText.text = $"Score: {_currentScore}";
+        }
+    }
+}
